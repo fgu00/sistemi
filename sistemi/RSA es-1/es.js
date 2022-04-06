37e982c49476d9a8becd19da02d5c6edc2579942
fgu00 <74243888+fgu00@users.noreply.github.com>
2022-04-06 11:46:47 +0200

    Add files via upload

---
+++ sistemi/RSA es-1/es.js
@@ -0,0 +1,19 @@
+var lettere=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,,w,x,y,z];
+const parola="RUSSO";
+var cifra=[];
+for(var a=0;a<parola.length;a++){
+for(var b=0;b<lettere.length;b++){
+    if(lettere[b]===parola.charAt(b)){
+        
+    }
+}
+}
+var cifratura=[];
+for(var a=0;a<cifra.length;a++){
+cifratura[a]=(cifra[a]**7)%55;
+console.log(cifratura[a]);
+}
+for(var a=0;a<cifra.length;a++){
+    cifratura[a]=(cifra[a]**7)%55;
+    console.log(cifratura[a]);
+    }
